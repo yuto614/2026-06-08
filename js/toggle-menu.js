8608d8afb25583cc3adba7b25af9ce3d6d5654c1
yuto614 <184593547+yuto614@users.noreply.github.com>
2026-06-22 14:14:53 +0900

    Resolve merge conflicts; consolidate menu styles

--- js/toggle-menu.js
+++ js/toggle-menu.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 window.addEventListener('load', function () {
     var $button = this.document.querySelector('.toggle-menu-button');
     var $menu = this.document.querySelector('.header-site-menu');
@@ -18,25 +17,4 @@ $(function () {
             {"top": $(window).scrollTop() + 100},
         500);
     });
-=======
-window.addEventListener('load', function () {
-    var $button = this.document.querySelector('.toggle-menu-button');
-    var $menu = this.document.querySelector('.header-site-menu');
-    $button.addEventListener('click', function () {
-        if ($menu.classList.contains('is-show')) {
-            $menu.classList.remove('is-show');
-        }
-        else {
-            $menu.classList.add('is-show');
-        }
-    });
-});
-
-$(function () {
-    $(window).scroll (function () {
-        $("nav.floating").stop().animate(
-            {"top": $(window).scrollTop() + 100},
-        500);
-    });
->>>>>>> b338ed4ca9d0b041e5816c2b3f47269ad994fbc7
 });
